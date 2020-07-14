5660b8f79c27b132355a44762b5660b8f1980b70
daolmo <67968940+daolmo@users.noreply.github.com>
2020-07-14 12:06:33 -0500

    First commit
    First commit

---
+++ Daniel_O_ControlFlowExercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+// setting up the loop to count to 180 seconds (3 min)
+for index in 0...180 {
+    print("\(index)")
+//setting up conditional statements for the timer to say while it runs
+if index == 180 {
+    print("Timer complete!")
+    }
+if index < 180 {
+        print("Timer in progress...")
+    }
+
+    
+}
+
